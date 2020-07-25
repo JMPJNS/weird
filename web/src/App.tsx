7b2164fe940a7f4b611e7676742b0a08643ec592
Jonas <jonas@jmp.blue>
2020-07-25 20:59:55 +0200

    exact route for home page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -21,6 +21,7 @@ export default function App() {
         </nav>
 
         <Route path="/free-robux" component={FreeRobux} />
+        <Route exact path="/" component={FakeText} />
         
       </main>
     </Router>
@@ -36,30 +37,6 @@ const meta = {
   description: "JMP's Weird Random Projects"
 }
 
-// Home Page
-const Home = () => (
-  <Fragment>
-    <h1>Home</h1>
-    <FakeText />
-  </Fragment>
-);
-// About Page
-const About = () => (
-  <Fragment>
-    <h1>About</h1>
-    <FakeText />
-  </Fragment>
-);
-// Contact Page
-const Contact = () => (
-  <Fragment>
-    <h1>Contact</h1>
-    <FakeText />
-  </Fragment>
-);
-
 const FakeText = () => (
-  <p>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-  </p>
+  <p>No You</p>
 )
