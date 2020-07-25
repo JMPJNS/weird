caf2ae9e02601b3c081004830f0f3ac6c5f209b2
Jonas <jonas@jmp.blue>
2020-07-25 21:37:55 +0200

    add dockerfile

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+﻿FROM node:12
+WORKDIR /usr/src/app
+
+COPY package*.json ./
+RUN npm install
+
+WORKDIR /usr/src/app/web
+
+COPY web/package*.json ./
+RUN npm install
+
+
+
+RUN npm run build
+
+WORKDIR /usr/src/app
+
+COPY . .
+EXPOSE 5000
+
+
+CMD ["npm", "run start"]
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
-﻿const t = "asdf"
+﻿import * as express from "express"
+import * as path from "path"
 
-console.log(t)
+const app = express()
+
+app.use(express.static(path.join(__dirname, "..", 'web/build')))
+
+app.get('/api', (req,res) => {
+	res.end("yes")
+})
+
+app.get('*', (req,res) =>{
+	res.sendFile(path.join(__dirname, "..", 'web/build/index.html'))
+})
+
+const port = process.env.PORT || 5000
+app.listen(port)
+
+console.log('Listening on ' + port)
